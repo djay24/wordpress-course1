4a2e88963bbc8e57f4a43b0271786cbc1a5f2d51
DJ Rodriguez <djayr24@gmail.com>
2018-10-18 19:58:47 -0700

    up the wordpress loop lesson

---
+++ index.php
@@ -0,0 +1,33 @@
+ <!-- lesson trying out functions -->
+<!-- <?php 
+function greet($name, $color) {
+    echo "<p>Hi, my name is $name and my favorite color is $color</p>";
+}
+
+greet('John', 'blue');
+greet('Jane', 'green');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p> -->
+
+
+<!-- lesson on arrays  -->
+<!-- <?php
+$names = array('Dj', 'Franklin', 'Jerry', 'Lamar');
+$count = 0;
+while($count < count($names)) {
+    echo "<li>Hi, my name is $names[$count]</li>";
+    $count++;
+};
+?> -->
+
+<!-- lesson on wordpress while loop -->
+<!-- <?php 
+    while(have_posts()) {
+        the_post(); ?>
+        <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+        <p><?php the_content();?></p>
+        <hr>
+ <?php };
+?> -->
